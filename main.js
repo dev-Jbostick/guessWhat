bdc24a4c8713ad2bb54d2a48d1185c66ec9e09c8
Jessica Bostick <jessicabostick@Fancy-Mac.local>
2020-03-12 16:39:50 -0500

    added game files{HTML, CSS, JS}

---
+++ main.js
@@ -0,0 +1,6 @@
+// console.log("This is it!");
+function removeElement() {
+    document.getElementById("tagLine").style.display = "none";
+    document.getElementById("instructions").style.display = "none";
+    document.getElementById("startButton").style.display = "none";
+  }
