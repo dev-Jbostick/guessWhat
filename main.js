3909c6439db60ca517ef06b5e766b3bbd2e2b203
Jessica Bostick <jessicabostick@Fancy-Mac.local>
2020-03-13 13:07:45 -0500

    added play space and player1 & 2div

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // console.log("This is it!");
+//Define Variables
+// This array element lists the options that players will have to choose from.
+// const letterPool = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+// const secretWord; //This will represent the word being guessed
+// const blanks;
+
+//get elements
+
+
+//this function hides the instructions, the tagline and the start button when a user click on Start.
 function removeElement() {
     document.getElementById("tagLine").style.display = "none";
     document.getElementById("instructions").style.display = "none";
